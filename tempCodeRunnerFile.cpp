0a5786f73c9d7d821add885e4e11a8c8639a545b
ishan-sp <ishan04722042@gmail.com>
2023-10-29 23:54:10 +0530

    pushed changes to repo

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -2,11 +2,8 @@
 using namespace std;
 
 int main() {
-    int i =5;
-    int &j = i;
-    int &k = j;
-    int &l = i;
-    cout<<i<<endl<<j<<endl<<k<<endl<<l<<endl;
+    int i = 5, j = 10;
+    cout <<i<<endl<<j; 
 
     return 0;
 }
